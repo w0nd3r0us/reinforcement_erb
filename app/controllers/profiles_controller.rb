6beccfa53d897422cba9fdff5ddf466bfc4e7ab3
w0nd3r0us <steven.grewal@gmail.com>
2018-06-27 00:55:15 -0400

    DRY'd up new.html.erb

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,11 @@
 class ProfilesController < ApplicationController
   def new
+    @infos = [
+      'email',
+      'username',
+      'pin',
+      'website',
+      'address',
+      'alias']
   end
 end
